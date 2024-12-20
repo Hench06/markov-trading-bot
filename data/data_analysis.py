0712bdd5bb56ee2b3665b29612d9c6171ae79b2d
Gautam Shankar <gsgaav@gmail.com>
2024-12-20 01:31:55 -0500

    data cleaned and ready for manipulation

--- data/data_analysis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Update the path to the file's location
-file_path = "data/ARM_stock_data.csv"  # Adjust if needed
-
-
-    # Load the CSV file
-data = pd.read_csv(file_path)
-print(data.head())
