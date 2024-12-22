00ffa43ad7679ab74861a35f097250e64db06e95
Gautam Shankar <gsgaav@gmail.com>
2024-12-22 00:08:58 -0500

    Create bot.py

---
+++ scripts/bot.py
@@ -0,0 +1,14 @@
+
+
+def trading_signal(predicted_state):
+    if predicted_state == "Increase":
+        return "Buy"
+    elif predicted_state == "Decrease":
+        return "Sell"
+    else:
+        return "Hold"
+    
+predicted_states =  ['Increase', 'Increase', 'Increase', 'Increase', 'No Change', 'Decrease']
+
+signals = [trading_signal(state) for state in predicted_states]
+print(signals)
