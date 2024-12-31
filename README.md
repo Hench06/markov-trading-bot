6cfd005a87e4f26190e53632d888a76fac1701aa
Gautam Shankar <gsgaav@gmail.com>
2024-12-31 01:00:14 -0500

    using hmmlearn library since i am able to generate more advanced models

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # markov-trading-bot
 A Markov chain-based trading bot for stock market predictions
+- Analyzed stock data from ARM and AAPL
+- used hmmlearn library to create a hidden markov model to predict future stock prices using x states
+
--- data/arm_stock_data.py
+++ data/arm_stock_data.py
@@ -3,7 +3,7 @@ import yfinance as yf
 # Define the stock ticker and date range
 stock_ticker = "ARM"  # ARM Holdings ticker
 start_date = "2022-01-01"  # Adjust the start date as needed
-end_date = "2024-12-01"  # Adjust the end date as needed
+end_date = "2024-12-30"  # Adjust the end date as needed
 
 # Fetch historical stock data
 try:
